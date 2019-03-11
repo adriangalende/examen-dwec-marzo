adfec8926557a318fc742296f2f2db6763e9d61f
Adrián <adriangalende@gmail.com>
2019-03-11 18:24:26 +0100

    2 historia -> pintar raffles

--- js/index.js
+++ js/index.js
@@ -105,7 +105,100 @@ window.onload = function(){
         }
     });
 
+    var raffleArray = function(){
+        var raffleArrayAux = [];
+        for(raffle in raffles){
+            raffleArrayAux.push(raffles[raffle]);
+        }
+        return raffleArrayAux;
+    }
 
+    //Todos los títulos de  las rifas que vienen en el objeto
+    var rafflesKeys = Object.keys(raffles)
+
+    rafflesKeys.forEach(raffleTitle => {
+        //Obtenemos el objeto raffle;
+       let raffle = raffles[raffleTitle];
+
+       var div = document.createElement('div');
+        div.id = raffleTitle.replace(' ','-');
+        div.classList.add('card');
+        div.classList.add('d-flex');
+        div.classList.add('p-3')
+        div.classList.add('m-3')
+
+
+        Object.keys(raffle).forEach(raffleKey => {
+
+            if(raffleKey == "logo"){
+                let img = document.createElement('img');
+                img.src = raffle[raffleKey];
+                img.classList.add('card-img-top')
+                div.appendChild(img);
+
+                let shop = document.createElement('small');
+                let shopName = document.createTextNode(raffleTitle);
+                shop.classList.add('font-weight-bold')
+                shop.classList.add('text-center')
+                shop.append(shopName);
+                div.appendChild(shop);
+
+
+            } else if(raffleKey == "url"){
+
+                let button = document.createElement('a');
+                button.href = raffle[raffleKey];
+                button.classList.add('btn');
+
+                let status = "";
+                if(raffle['Closes'] == "closed"){
+                    status = "CLOSED";
+                    button.classList.add(raffle['Closes']);
+                } else {
+                    status = raffle['Opens'] == "live" ? "ENTER RAFFLE" : "ANNOUNCED";
+                    button.classList.add(raffle['Opens']);
+                }
+
+                let buttonText = document.createTextNode(status);
+
+                button.append(buttonText);
+                div.appendChild(button);
+
+            } else {
+                let p = document.createElement('p');
+                let text = document.createTextNode( raffleKey +" - "+ raffle[raffleKey]);
+                p.append(text);
+                div.appendChild(p);
+            }
+        })
+
+
+
+        let star_container = document.createElement('div');
+        star_container.classList.add('font-weight-bold')
+        star_container.classList.add('d-flex')
+        star_container.classList.add('p-3')
+        star_container.classList.add('align-items-center')
+
+        let star = document.createElement('input');
+        star.type ="checkbox";
+        star.classList.add('ml-2')
+
+        let star_container_text = document.createTextNode("Mark as read");
+
+
+
+        star.classList.add('fa')
+        star.classList.add('fa-star')
+
+        star_container.append(star_container_text);
+        star_container.append(star);
+        div.append(star_container);
+
+        document.getElementById('raffles-container').append(div);
+
+
+    });
 
 
 }
